8d78b6a27e2c100ae073c0616a678a0e8caf0c23
Mac <mac@Taglines-iMac-Pro-33.local>
2024-05-01 10:31:14 +0530

    defining functions directly without class

--- question_one.py
+++ question_one.py
@@ -4,42 +4,37 @@ print('\nA. Length of the list \nB. Display first 3 numbers \nC. Display sum of 
 
 option=input().capitalize()
     
-class UserFunction:
+def listLenght():
+    print('Length of The List : '+ str(len(numbers)))        
     
-    def listLenght():
-        print('Length of The List : '+ str(len(numbers)))        
-    
-    def firstThreeNumbers():
-        #print('1: {}, 2: {}, 3: {}'.format(numbers[0],numbers[1],numbers[2]))
-        print('First 3 Numbers : '+str(numbers[:3]))
+def firstThreeNumbers():
+    #print('1: {}, 2: {}, 3: {}'.format(numbers[0],numbers[1],numbers[2]))
+    print('First 3 Numbers : '+str(numbers[:3]))
 
-    def oddNumberSum():
-        oddsum = sum(i for i in numbers if i%2)
-        print('Sum of Odd Numbers : '+str(oddsum))
+def oddNumberSum():
+    oddsum = sum(i for i in numbers if i%2)
+    print('Sum of Odd Numbers : '+str(oddsum))
     
-    def countDupNumbers():
-        dup = {x for x in numbers if numbers.count(x) > 1}       
-        print('Number of Duplicate Numbers : '+str(len(dup)))
+def countDupNumbers():
+    dup = {x for x in numbers if numbers.count(x) > 1}       
+    print('Number of Duplicate Numbers : '+str(len(dup)))
         
-    def listWithoutDupNumers():
-        res=[]
-        [res.append(x) for x in numbers if x not in res]
-        print('List Without Duplicate Numbers : '+str(res))
-        
-        
-func=UserFunction
+def listWithoutDupNumers():
+    res=[]
+    [res.append(x) for x in numbers if x not in res]
+    print('List Without Duplicate Numbers : '+str(res))
 
 def identifier(option) :
     if option == 'A':
-        func.listLenght()
+        listLenght()
     elif option=='B':
-        func.firstThreeNumbers()
+        firstThreeNumbers()
     elif option=='C':
-        func.oddNumberSum()
+        oddNumberSum()
     elif option=='D':
-        func.countDupNumbers()
+        countDupNumbers()
     elif option=='E':
-        func.listWithoutDupNumers()
+        listWithoutDupNumers()
     else : print('Please Select Correct Option....')
     
 identifier(option)
