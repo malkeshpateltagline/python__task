2dbe8b94e8a2035da61dc6c3f82aee167f656c84
Mac <mac@Taglines-iMac-Pro-33.local>
2024-05-06 10:25:00 +0530

    removed extra spaces, rename function to common_ratio, added correct example

--- question_eleven.py
+++ question_eleven.py
@@ -17,7 +17,7 @@ ap = correct_ap(ap_seq, common_diff)
 print("A.P. =", ap)
 
 
-def common_gp_diff(seq):
+def common_ratio(seq):
     return seq[1] / seq[0]
 
 def correct_gp(seq, common_diff):
@@ -26,25 +26,10 @@ def correct_gp(seq, common_diff):
         correct_se.append(math.trunc(correct_se[-1] * common_diff))
     return correct_se
 
-gp_seq = [3, 6, 9, 12, 241, 96]
+gp_seq = [3, 6, 9, 12, 16, 18]
 
-common_gp_diff = common_gp_diff(gp_seq)
+common_ratio = common_ratio(gp_seq)
 
-gp = correct_gp(gp_seq, common_gp_diff)
+gp = correct_gp(gp_seq, common_ratio)
 
 print("G.P. =", gp)
-
-# a_p= [2, 5, 8, 11, 15, 17] 
-# g_p = [3, 6, 9, 12, 16, 18]
-# lst=[]
-# for i in range(len(a_p)-1):
-#     pre_interval = a_p[i+1]-a_p[i]
-#     lst.append(pre_interval)
-
-# lsttoset = set(lst)    
-
-# if len(lsttoset)>1:
-#     print('make chnages to list')
-#     print(lst)
-# else:
-#     print(a_p)
